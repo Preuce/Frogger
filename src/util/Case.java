0329280c969b63a1ceadc09aee1586514adeaac3
Paul <paul.hercouet@u-psud.fr>
2020-12-07 00:40:07 +0100

    la partie 2 pas encore finie de Paul (j'avance lentement mais surement)
    Signed-off-by: Paul <paul.hercouet@u-psud.fr>

--- src/util/Case.java
+++ src/util/Case.java
@@ -1,11 +1,10 @@
 package util;
 
 public class Case {
-	public final int absc;
+	public int absc;
 	public final int ord;
 	
 	public Case(int absc, int ord) {
-		super();
 		this.absc = absc;
 		this.ord = ord;
 	}
